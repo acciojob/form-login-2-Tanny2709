5bba4757005f3dfa3276a5f81e9f2b4c46221098
Tanny2709 <123792042+Tanny2709@users.noreply.github.com>
2025-11-11 17:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+// your JS code here. If required
+document.getElementById("userForm").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const first = document.getElementById("firstName").value;
+    const last = document.getElementById("lastName").value;
+    const phone = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("emailId").value;
+
+    alert(
+        "First Name: " + first + " " +
+        "Last Name: " + last + " " +
+        "Phone Number: " + phone + " " +
+        "Email ID: " + email
+    );
+});
